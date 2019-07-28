13d69b0c65642651bf7fedb6a56614d7b727f783
asardaes <alexis.sarda@gmail.com>
2019-07-28 23:31:02 +0200

    Merge headers

---
+++ src/core/columns.cpp
@@ -0,0 +1,74 @@
+#include "columns.h"
+
+#include <typeinfo>
+#include <typeindex>
+#include <utility> // std::move
+
+namespace wiserow {
+
+// =================================================================================================
+
+ColumnCollectionMetadata::ColumnCollectionMetadata(const Rcpp::List& metadata)
+    : input_class(get_string(metadata, "input_class"))
+    , output_mode(std::move(parse_type(get_string(metadata, "output_mode"))))
+{
+    Rcpp::StringVector in_modes = metadata["input_modes"];
+    for (R_xlen_t i = 0; i < in_modes.length(); i++) {
+        std::string in_mode = Rcpp::as<std::string>(in_modes(i));
+        input_modes.push_back(std::move(parse_type(in_mode)));
+    }
+}
+
+std::string ColumnCollectionMetadata::get_string(const Rcpp::List& metadata, const std::string& key) {
+    return Rcpp::as<std::string>(metadata[key]);
+}
+
+std::type_index ColumnCollectionMetadata::parse_type(const std::string& type_str) {
+    if (type_str == "integer") {
+        return std::type_index(typeid(int));
+    }
+    else if (type_str == "double") {
+        return std::type_index(typeid(double));
+    }
+    else {
+        Rcpp::stop("[wiserow] unsupported type: " + type_str);
+    }
+}
+
+// =================================================================================================
+
+ColumnCollection ColumnCollection::coerce(ColumnCollectionMetadata metadata, SEXP data) {
+    if (metadata.input_class == "matrix") {
+        if (metadata.input_modes[0] == typeid(int)) {
+            return MatrixColumnCollection<Rcpp::IntegerMatrix>(data);
+        }
+        else {
+            return MatrixColumnCollection<Rcpp::NumericMatrix>(data);
+        }
+    }
+    else {
+        Rcpp::stop("[wiserow] unsupported input class: " + metadata.input_class);
+    }
+}
+
+ColumnCollection::ColumnCollection(const std::size_t nrow)
+    : nrow_(nrow)
+{ }
+
+std::size_t ColumnCollection::ncol() const {
+    return columns_.size();
+}
+
+std::size_t ColumnCollection::nrow() const {
+    return nrow_;
+}
+
+const VariantColumn& ColumnCollection::operator[](const std::size_t j) const {
+    return *(columns_[j]);
+}
+
+const supported_col_t ColumnCollection::operator()(const std::size_t i, const std::size_t j) const {
+    return (*(columns_[j]))[i];
+}
+
+} // namespace wiserow
--- src/core/columns.h
+++ src/core/columns.h
@@ -1,13 +1,20 @@
 #ifndef WISEROW_COLUMNS_H_
 #define WISEROW_COLUMNS_H_
 
-#include <cstddef> // std::size_t
-#include <stdexcept>
+#include <cstddef> // size_t
+#include <memory> // shared_ptr
+#include <stdexcept> // out_of_range
 #include <string>
+#include <type_traits> // conditional, is_same
+#include <typeindex>
+#include <vector>
 
-#include "types.h"
+#include <Rcpp.h>
+#include <boost/variant/variant.hpp>
 
 namespace wiserow {
+
+typedef boost::variant<int, double> supported_col_t;
 
 // =================================================================================================
 
@@ -43,6 +50,62 @@ public:
 private:
     T const * const data_ptr_;
     const std::size_t size_;
+};
+
+// =================================================================================================
+
+class ColumnCollectionMetadata {
+public:
+    ColumnCollectionMetadata(const Rcpp::List& metadata);
+
+    const std::string input_class;
+    const std::type_index output_mode;
+
+    std::vector<std::type_index> input_modes;
+
+private:
+    static std::string get_string(const Rcpp::List& metadata, const std::string& key);
+    static std::type_index parse_type(const std::string& type_str);
+};
+
+// =================================================================================================
+
+class ColumnCollection
+{
+public:
+    static ColumnCollection coerce(ColumnCollectionMetadata metadata, SEXP data);
+
+    ColumnCollection(const std::size_t nrow);
+    virtual ~ColumnCollection() {}
+
+    std::size_t nrow() const;
+    std::size_t ncol() const;
+
+    const VariantColumn& operator[](const std::size_t j) const;
+    const supported_col_t operator()(const std::size_t i, const std::size_t j) const;
+
+protected:
+    std::vector<std::shared_ptr<const VariantColumn>> columns_;
+    const std::size_t nrow_;
+};
+
+// =================================================================================================
+
+template<typename T>
+class MatrixColumnCollection : public ColumnCollection
+{
+public:
+    MatrixColumnCollection(const T& mat)
+        : ColumnCollection(mat.nrow())
+    {
+        typedef typename std::conditional<std::is_same<T, Rcpp::IntegerMatrix>::value, int, double>::type OUT;
+
+        for (int j = 0; j < mat.ncol(); j++) {
+            columns_.push_back(
+                std::make_shared<SurrogateColumn<OUT>>(&mat[static_cast<std::size_t>(j) * nrow_], nrow_)
+            );
+        }
+    }
 };
 
 } // namespace wiserow
