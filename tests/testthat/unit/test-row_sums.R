9260e490fcd5405cdbdbc4f78944aab577caf3aa
asardaes <alexis.sarda@gmail.com>
2019-08-03 22:35:17 +0200

    Add test

--- tests/testthat/unit/test-row_sums.R
+++ tests/testthat/unit/test-row_sums.R
@@ -13,9 +13,8 @@ test_that("row_sums can handle errors and edge cases appropriately.", {
         expect_error(regexp = "not support", row_sums(char_mat, "complex"))
     )
 
-    suppressWarnings(
-        expect_error(regexp = "row_sums can only return", row_sums(int_mat, "character"))
-    )
+    expect_error(regexp = "row_sums can only return", row_sums(int_mat, "character"))
+    expect_error(regexp = "row_sums can only return", row_sums(int_mat, "character", output_class = "list"))
 
     expect_identical(row_sums(as.matrix(data.frame())), vector("integer"))
 })
