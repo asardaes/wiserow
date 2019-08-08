1d0812c631a4b86ed5667547553e6298ec1a3134
asardaes <alexis.sarda@gmail.com>
2019-08-08 21:26:35 +0200

    Refactor visit_into_numeric

--- src/exports/numeric_visit.h
+++ src/exports/numeric_visit.h
@@ -14,46 +14,42 @@ namespace wiserow {
 
 // =================================================================================================
 
-template<template<typename> class Worker>
-void visit_into_numeric_vector(const char* fun_name, const OperationMetadata& metadata, SEXP data, SEXP output) {
+template<typename Worker, typename Wrapper>
+void visit_into_numeric(const OperationMetadata& metadata,
+                        const ColumnCollection& col_collection,
+                        SEXP output,
+                        const std::size_t out_len)
+{
+    if (out_len == 0) return;
+
+    Wrapper wrapper(output);
+    Worker worker(metadata, col_collection, wrapper);
+    parallel_for(worker);
+}
+
+// -------------------------------------------------------------------------------------------------
+
+template<template<typename> class Worker, template<int, typename> class Wrapper>
+void visit_into_numeric(const char* fun_name, const OperationMetadata& metadata, SEXP data, SEXP output) {
     ColumnCollection col_collection = ColumnCollection::coerce(metadata, data);
     std::size_t out_len = output_length(metadata, col_collection);
 
     switch(metadata.output_mode) {
     case INTSXP: {
-        if (out_len == 0) break;
-
-        VectorOutputWrapper<INTSXP, int> wrapper(output);
-        Worker<int> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
+        visit_into_numeric<Worker<int>, Wrapper<INTSXP, int>>(metadata, col_collection, output, out_len);
         break;
     }
     case REALSXP: {
-        if (out_len == 0) break;
-
-        VectorOutputWrapper<REALSXP, double> wrapper(output);
-        Worker<double> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
+        visit_into_numeric<Worker<double>, Wrapper<REALSXP, double>>(metadata, col_collection, output, out_len);
         break;
     }
     case LGLSXP: {
-        if (out_len == 0) break;
-
-        VectorOutputWrapper<LGLSXP, int> wrapper(output);
-        Worker<int> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
+        visit_into_numeric<Worker<int>, Wrapper<LGLSXP, int>>(metadata, col_collection, output, out_len);
         break;
     }
     case CPLXSXP: {
-        if (out_len == 0) break;
-
-        VectorOutputWrapper<CPLXSXP, std::complex<double>> wrapper(output);
-        Worker<std::complex<double>> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
+        visit_into_numeric<Worker<std::complex<double>>, Wrapper<CPLXSXP, std::complex<double>>>(
+                metadata, col_collection, output, out_len);
         break;
     }
     default: {
@@ -65,57 +61,16 @@ void visit_into_numeric_vector(const char* fun_name, const OperationMetadata& me
 // -------------------------------------------------------------------------------------------------
 
 template<template<typename> class Worker>
-void visit_into_enlisted_numeric_vector(const char* fun_name, const OperationMetadata& metadata, SEXP data, SEXP output) {
-    ColumnCollection col_collection = ColumnCollection::coerce(metadata, data);
-    std::size_t out_len = output_length(metadata, col_collection);
-
-    switch(metadata.output_mode) {
-    case INTSXP: {
-        if (out_len == 0) break;
-
-        ListOutputWrapper<INTSXP, int> wrapper(output);
-        Worker<int> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
-        break;
-    }
-    case REALSXP: {
-        if (out_len == 0) break;
-
-        ListOutputWrapper<REALSXP, double> wrapper(output);
-        Worker<double> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
-        break;
-    }
-    case CPLXSXP: {
-        if (out_len == 0) break;
-
-        ListOutputWrapper<CPLXSXP, std::complex<double>> wrapper(output);
-        Worker<std::complex<double>> worker(metadata, col_collection, wrapper);
-
-        parallel_for(worker);
-        break;
-    }
-    default: {
-        Rcpp::stop("[wiserow] %s can only return integers, doubles, or complex numbers.", fun_name);
-    }
-    }
-}
-
-// =================================================================================================
-
-template<template<typename> class Worker>
 SEXP visit_into_numeric(const char* fun_name, SEXP m, SEXP data, SEXP output) {
     OperationMetadata metadata(m);
 
     switch(metadata.output_class) {
     case OutputClass::VECTOR: {
-        visit_into_numeric_vector<Worker>(fun_name, metadata, data, output);
+        visit_into_numeric<Worker, VectorOutputWrapper>(fun_name, metadata, data, output);
         break;
     }
     case OutputClass::LIST: {
-        visit_into_enlisted_numeric_vector<Worker>(fun_name, metadata, data, output);
+        visit_into_numeric<Worker, ListOutputWrapper>(fun_name, metadata, data, output);
         break;
     }
     default: { // nocov start
